149dd294b280fe22103288ab06209c92885640b7
f0rget <f0rget@pm.me>
2020-09-11 20:35:59 +0300

    aoc-new.sh also copy template and add build entry

--- aoc-new.sh
+++ aoc-new.sh
@@ -4,9 +4,16 @@ YEAR=$1
 DAY=$2
 
 DIR="src/$YEAR/day$DAY"
+MAIN_FILE=$DIR/main.rs
 echo "Creating new dir"
 mkdir -p $DIR
 
-#TODO: cp template
+cat >> Cargo.toml <<EOL
+[[bin]]
+name = "${YEAR}day${DAY}"
+path = "$MAIN_FILE"
+EOL
+
+sed "s/<YEAR>/$YEAR/g;s/<DAY>/$DAY/g" src/templates/main.rs > $MAIN_FILE
 
 aocdl -day $DAY -year=$YEAR -output "$DIR/input"
---
+++ src/templates/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let filename = "src/<YEAR>/day<DAY>/input";
+    let cont = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let res1 = part1(&cont);
+    println!("part1 :\n{}", res1);
+    //assert_eq!(res1, 0);
+
+    let res2 = part2(&cont);
+    println!("part2 :\n{}", res2);
+    //assert_eq!(res2, 0);
+}
+
+fn part1(cont: &str) -> u16 {
+    0
+}
+
+fn part2(cont: &str) -> u16 {
+    0
+}
